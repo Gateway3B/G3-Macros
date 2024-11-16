eff91dceaf2d9e8e1bd3b79b7ec2a97f2acbb23a
Matthew Weisfeld <matthewedwardweisfeld@gmail.com>
2024-11-16 00:32:37 -0500

    Alt on colemak layer, keymap visualizer integration, colemak_control on right pinky on mouse layer, obs settings.

--- ZSA Voyager/keymap.c
+++ ZSA Voyager/keymap.c
@@ -3,7 +3,7 @@
 #include "keymap_us_international.h"
 
 enum custom_keycodes {
-    RGB_SLD = ML_SAFE_RANGE,
+    RGB_SLD = SAFE_RANGE,
     MAC_MISSION_CONTROL,
     MAC_SPOTLIGHT,
     MAC_LOCK,
@@ -31,38 +31,38 @@ enum voyager_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [COLEMAK] = LAYOUT_voyager(
-        KC_ESCAPE,         QWERTY_ON, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, QWERTY_OFF,          MAGIC_SWAP_LCTL_LGUI,  KC_MS_BTN4,    KC_PSCREEN, KC_MS_BTN5, MAGIC_UNSWAP_LCTL_LGUI,   MAC_SPOTLIGHT,
-        KC_LGUI ,            KC_Q,    KC_W,            KC_F,                KC_P,                KC_G,                KC_J,                  KC_L,          KC_U,       KC_Y,       KC_BSPACE, KC_LALT,
-        TAB_LEFT,            KC_A,    KC_R,            KC_S,                KC_T,                KC_D,                KC_H,                  KC_N,          KC_E,       KC_I,       KC_O,      TAB_RIGHT,
-        APP_CYCLE,           KC_Z,    KC_X,            KC_C,                KC_V,                KC_B,                KC_K,                  KC_M,          KC_COMMA,   KC_DOT,     KC_QUOTE,  APP_CYCLE_CONTROL,
-                                                                            KC_LCTRL,            KC_LSHIFT,           LT(NUMBERS, KC_SPACE), MO(NAVIGATION)
+        KC_ESCAPE,         QWERTY_ON, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, KC_LALT,             QK_MAGIC_SWAP_LCTL_LGUI, KC_MEH,      KC_PSCR,    KC_HYPR,    QK_MAGIC_UNSWAP_LCTL_LGUI, MAC_SPOTLIGHT,
+        KC_LGUI ,            KC_Q,    KC_W,            KC_F,                KC_P,                KC_G,                KC_J,                    KC_L,        KC_U,       KC_Y,       KC_BSPC,                   KC_LALT,
+        TAB_LEFT,            KC_A,    KC_R,            KC_S,                KC_T,                KC_D,                KC_H,                    KC_N,        KC_E,       KC_I,       KC_O,                      TAB_RIGHT,
+        APP_CYCLE,           KC_Z,    KC_X,            KC_C,                KC_V,                KC_B,                KC_K,                    KC_M,        KC_COMMA,   KC_DOT,     KC_QUOTE,                  APP_CYCLE_CONTROL,
+                                                                            KC_LCTL,            KC_LSFT,              LT(NUMBERS, KC_SPACE),   MO(NAVIGATION)
     ),
     [QWERTY] = LAYOUT_voyager(
-        KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_TRANSPARENT,               KC_5,           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,
+        KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           QWERTY_OFF,                   KC_5,           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,
         APP_CYCLE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                         KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT,
-        KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                         KC_H,           KC_J,           KC_K,           KC_L,           KC_BSPACE,      KC_ENTER,
-        KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                         KC_N,           KC_M,           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                        KC_SPACE, KC_LCTRL,                           KC_TRANSPARENT, KC_TRANSPARENT
+        KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                         KC_H,           KC_J,           KC_K,           KC_L,           KC_BSPC,        KC_ENTER,
+        KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                         KC_N,           KC_M,           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                                                                        KC_SPACE,       KC_LCTL,                      KC_TRANSPARENT, KC_TRANSPARENT
     ),
     [NAVIGATION] = LAYOUT_voyager(
         KC_F1,          KC_F2,     KC_F3,   KC_F4,   KC_F5,          KC_F6,                                           KC_F7,          KC_F8,         KC_F9,   KC_F10,         KC_F11,    KC_F12,
-        KC_TRANSPARENT, KC_ESCAPE, KC_DLR,  KC_HASH, KC_ASTR,        KC_AMPR,                                         KC_HOME,        LCTL(KC_LEFT), KC_UP,   LCTL(KC_RIGHT), KC_BSPACE, KC_TRANSPARENT,
-        KC_TRANSPARENT, KC_TAB,    KC_LABK, KC_LPRN, KC_LCBR,        KC_LBRACKET,                                     KC_END,         KC_LEFT,       KC_DOWN, KC_RIGHT,       KC_ENTER,  KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_ESCAPE, KC_DLR,  KC_HASH, KC_ASTR,        KC_AMPR,                                         KC_HOME,        LCTL(KC_LEFT), KC_UP,   LCTL(KC_RIGHT), KC_BSPC,   KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TAB,    KC_LABK, KC_LPRN, KC_LCBR,        KC_LBRC,                                         KC_END,         KC_LEFT,       KC_DOWN, KC_RIGHT,       KC_ENTER,  KC_TRANSPARENT,
         KC_TRANSPARENT, KC_LALT,   KC_F10,  KC_F11,  KC_F12,         KC_GRAVE,                                        KC_PLUS,        KC_MINUS,      KC_UNDS, KC_EQUAL,       KC_SLASH,  KC_TRANSPARENT,
                                                      KC_TRANSPARENT, KC_TRANSPARENT,                                  KC_TRANSPARENT, KC_TRANSPARENT
     ),
     [NUMBERS] = LAYOUT_voyager(
         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,               KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RESET,
-        KC_TRANSPARENT, KC_ESCAPE,      KC_PERC,        KC_AT,          KC_CIRC,        KC_PIPE,                      KC_MINUS,       KC_7,           KC_8,           KC_9,           KC_BSPACE,      KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_ESCAPE,      KC_PERC,        KC_AT,          KC_CIRC,        KC_PIPE,                      KC_MINUS,       KC_7,           KC_8,           KC_9,           KC_BSPC,        KC_TRANSPARENT,
         KC_TRANSPARENT, KC_TAB,         KC_DOT,         KC_COMMA,       KC_SLASH,       KC_ASTR,                      KC_PLUS,        KC_4,           KC_5,           KC_6,           KC_ENTER,       KC_TRANSPARENT,
         KC_TRANSPARENT, KC_LALT,        KC_F5,          KC_F1,          KC_F2,          KC_SPACE,                     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_0,           KC_TRANSPARENT,
                                                                         KC_TRANSPARENT, KC_TRANSPARENT,               KC_TRANSPARENT, KC_TRANSPARENT
     ),
     [MOUSE] = LAYOUT_voyager(
         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,      KC_TRANSPARENT,          KC_TRANSPARENT, KC_TRANSPARENT,       KC_TRANSPARENT, KC_TRANSPARENT,       KC_TRANSPARENT, KC_TRANSPARENT,
-        KC_TRANSPARENT, KC_ESCAPE,      KC_MS_WH_LEFT,  KC_MS_WH_UP,    KC_MS_WH_RIGHT,      KC_PAUSE,                KC_MS_BTN5,     LGUI(LCTL(KC_LEFT)),  KC_MS_UP,       LGUI(LCTL(KC_RIGHT)), KC_BSPACE,      KC_TRANSPARENT,
-        KC_TRANSPARENT, KC_MS_ACCEL2,   KC_MS_ACCEL1,   KC_MS_WH_DOWN,  KC_MS_ACCEL0,        KC_SCROLLLOCK,           KC_MS_BTN4,     KC_MS_LEFT,           KC_MS_DOWN,     KC_MS_RIGHT,          KC_ENTER,       KC_TRANSPARENT,
-        KC_TRANSPARENT, KC_LCTRL,       KC_LGUI,        KC_LALT,        MAC_MISSION_CONTROL, KC_SYSTEM_SLEEP,         MAC_LOCK,       MAGIC_TOGGLE_CTL_GUI, KC_RALT,        KC_RGUI,              KC_RCTRL,       KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_ESCAPE,      KC_MS_WH_LEFT,  KC_MS_WH_UP,    KC_MS_WH_RIGHT,      KC_PAUSE,                KC_MS_BTN5,     LGUI(LCTL(KC_LEFT)),  KC_MS_UP,       LGUI(LCTL(KC_RIGHT)), KC_BSPC,        KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_MS_ACCEL2,   KC_MS_ACCEL1,   KC_MS_WH_DOWN,  KC_MS_ACCEL0,        KC_SCROLL_LOCK,          KC_MS_BTN4,     KC_MS_LEFT,           KC_MS_DOWN,     KC_MS_RIGHT,          COLEMAK_CONTROL,KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_LCTL,        KC_LGUI,        KC_LALT,        MAC_MISSION_CONTROL, KC_SYSTEM_SLEEP,         MAC_LOCK,       QK_MAGIC_TOGGLE_CTL_GUI, KC_RALT,     KC_RGUI,              KC_ENTER,       KC_TRANSPARENT,
                                                                         COLEMAK_CONTROL,     KC_TRANSPARENT,          KC_MS_BTN1,     KC_MS_BTN2
     ),
 };
@@ -93,8 +93,8 @@ const key_override_t ctrl_equals_nokey_key_override =       ko_make_basic(MOD_MA
 const key_override_t forwardslash_backslash_key_override =  ko_make_basic(MOD_MASK_SHIFT, KC_SLASH,                 KC_BACKSLASH);                 // / backslash
 const key_override_t backspace_delete_key_override =        ko_make_with_layers(MOD_MASK_SHIFT, KC_BSPC,            KC_DEL, (1 << NAVIGATION));    // <- ->
 
-const key_override_t up_pgup_key_override =                 ko_make_with_layers_and_negmods(MOD_MASK_CTRL, KC_UP,   KC_PAGE_UP, (1 << NAVIGATION), MOD_MASK_ALT); // ctrl(up) page_up
-const key_override_t down_pgdown_key_override =             ko_make_with_layers_and_negmods(MOD_MASK_CTRL, KC_DOWN, KC_PAGE_DOWN, (1 << NAVIGATION), MOD_MASK_ALT); // ctrl(down) page_down
+const key_override_t to_bracket_key_override =              ko_make_with_layers(MOD_MASK_CTRL, KC_UP,               RCS(KC_BACKSLASH), (1 << NAVIGATION)); // ctrl(up) jump to bracket
+// const key_override_t down_pgdown_key_override =             ko_make_with_layers_and_negmods(MOD_MASK_CTRL, KC_DOWN, KC_PAGE_DOWN, (1 << NAVIGATION), MOD_MASK_ALT); // ctrl(down) page_down
 
 
 // MOUSE
@@ -128,8 +128,8 @@ const key_override_t **key_overrides = (const key_override_t *[]) {
     &forwardslash_backslash_key_override,
     &backspace_delete_key_override,
 
-    &up_pgup_key_override,
-    &down_pgdown_key_override,
+    &to_bracket_key_override,
+    // &down_pgdown_key_override,
 
     // MOUSE
     &volumeup_volumedown_key_override,
@@ -155,7 +155,7 @@ enum combos {
 
 const uint16_t PROGMEM comma_dot_combo[] = {KC_COMMA, KC_DOT, COMBO_END};
 const uint16_t PROGMEM space_layer_combo[] = {LT(NUMBERS, KC_SPACE), MO(NAVIGATION), COMBO_END};
-const uint16_t PROGMEM control_space_combo[] = {KC_SPACE, KC_LCTRL, COMBO_END};
+const uint16_t PROGMEM control_space_combo[] = {KC_SPACE, KC_LCTL, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     [COMMA_DOT_EXCLAIM] = COMBO(comma_dot_combo, KC_EXCLAIM),
@@ -236,6 +236,7 @@ uint8_t coords_to_led_pin(uint8_t row, uint8_t col) {
     return temp + (row * 6) + col;
 }
 
+const uint8_t DRIVER_LED_TOTAL = 52;
 static uint8_t LAST_KEY = DRIVER_LED_TOTAL;
 
 static uint8_t SWAPPED_GUI_CTRL = 26;
@@ -250,7 +251,7 @@ void keyboard_post_init_user(void) {
     rgb_matrix_enable();
 }
 
-const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
+const uint8_t PROGMEM ledmap[][52][3] = {
     [COLEMAK] = { {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255} },
 
     [NAVIGATION] = { {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255}, {169,255,255} },
@@ -292,8 +293,8 @@ void set_layer_color(int layer) {
     }
 }
 
-void rgb_matrix_indicators_user(void) {
-    if (keyboard_config.disable_layer_led) { return; }
+bool rgb_matrix_indicators_user(void) {
+    if (keyboard_config.disable_layer_led) { return true; }
     switch (biton32(layer_state)) {
         case COLEMAK:
             set_layer_color(COLEMAK);
@@ -312,6 +313,7 @@ void rgb_matrix_indicators_user(void) {
                 rgb_matrix_set_color_all(0, 0, 0);
             break;
     }
+    return true;
 }
 
 
@@ -351,11 +353,35 @@ void unregister_gui(void) {
     }
 }
 
+static bool COLEMAK_CONTROL_ACTIVE = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // if (
+    //     !COLEMAK_CONTROL_ACTIVE &&
+    //     get_highest_layer(layer_state) == COLEMAK &&
+    //     record->event.key.col == 4 && record->event.key.row == 8
+    // ) {
+    //     if (record->event.pressed) {
+    //         register_code(KC_O);
+    //     } else {
+    //         unregister_code(KC_O);
+    //     }
+    //     return true;
+    // }
+
     if (!(record->event.pressed)) {
-        if (record->event.key.col == 0 && record->event.key.row == 5) {
-            unregister_ctrl();
-            clear_mods();
+        if (COLEMAK_CONTROL_ACTIVE) {
+            if (record->event.key.col == 0 && record->event.key.row == 5) { // KC_CTRL
+                unregister_ctrl();
+                clear_mods();
+                COLEMAK_CONTROL_ACTIVE = false;
+
+            }
+            if (record->event.key.col == 4 && record->event.key.row == 8) { // KC_O
+                unregister_ctrl();
+                clear_mods();
+                COLEMAK_CONTROL_ACTIVE = false;
+            }
         }
         return true;
     }
@@ -387,7 +413,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case COLEMAK_CONTROL:
             layer_off(MOUSE);
             register_ctrl();
+            COLEMAK_CONTROL_ACTIVE = true;
 
+            return false;
             break;
 
         case QWERTY_ON:
@@ -423,10 +451,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case TAB_RIGHT:
             register_code(KC_LALT);
-            register_code(KC_PGDOWN);
+            register_code(KC_PGDN);
 
             unregister_code(KC_LALT);
-            unregister_code(KC_PGDOWN);
+            unregister_code(KC_PGDN);
 
             break;
 
